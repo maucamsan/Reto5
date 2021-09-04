e80b681e26cf79650496a8cf62e86092eccc6514
maucamsan <47923785+maucamsan@users.noreply.github.com>
2021-09-04 10:44:25 -0500

    Update VendidoDAO.java
    Consulta añadida a VendidoDAO

--- Reto5Parejas/src/dao/VendidoDAO.java
+++ Reto5Parejas/src/dao/VendidoDAO.java
@@ -5,10 +5,50 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.VendidoModel;
+import utils.ConnectionDB;
+
 /**
  *
  * @author lenovo
  */
 public class VendidoDAO {
+    private Connection connection = null;
+    
+    public void getvendido(int prodCodigo)
+    {
+        ArrayList<VendidoModel> ventas = new ArrayList();
+        try
+        {
+            if(connection == null)
+                connection = ConnectionDB.getConnection();
+            
+            String sql = "SELECT  prod_codigo, ven_nro_factura FROM vendido WHERE prod_codigo =?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, prodCodigo);
+            
+            ResultSet result = statement.executeQuery();
+            
+            while(result.next())
+            {
+                VendidoModel vendido = new VendidoModel(result.getInt(1), result.getInt(2));
+                
+                ventas.add(vendido);
+                
+            }
+            
+            
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("No se puede consultar la venta");
+        }
+    }
     
 }
